efe184c8ff39b04cd37726336676f3448b13ef00
gitv <email>
2017-05-05 20:00:51 +0200

    learn and repeat js and css

--- public/javascripts/jsblank.js
+++ public/javascripts/jsblank.js
@@ -1,8 +1,1 @@
 'use strict'
-let input = document.getElementsByClassName("Input")[0]
-let output = document.getElementsByClassName("Output")[0]
-
-let fruits = ["Strawberry", "Banana","Mango"]
-input.innerHTML += fruits
-
-output.innerHTML += "Index of Banana is " + fruits.indexOf("Banana")
